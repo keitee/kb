936d7488053b5fc68dd312f06acef84d76659562
Kit Park <kyoupark@cisco.com>
2018-04-09 13:16:53 +0100

    Mon  9 Apr 13:16:53 BST 2018 fh

--- bin/cpfromy.py
+++ bin/cpfromy.py
@@ -14,14 +14,18 @@ if __name__ == "__main__":
     logfile = "LOGlastrun_realtime"
 
     # to support:
-    # http://theyard.cisco.com/si_logs/translation/darwin_783e53899c98_02c09aa321ff337bb69eecf938b66444/index.html
-    # * http://theyard.cisco.com/si_logs/translation/darwin_783e53899c98_02c09aa321ff337bb69eecf938b66444/index.html
+    # http://theyard.cisco.com/si_logs/translation/darwin_783e53fe4a60_81b952e11b7e50199c8d07a4649310c7/index.html
+    # darwin_783e53ffaa6d_9276a4c871ae42a6a41f729def322c3f.tgz
 
-    match = re.match('.*/(darwin_.*_.*)/.*', sys.argv[1])
+    # why do not cover both?
+    # match = re.match('(darwin_[0-9A-Za-z]{12}_[0-9A-Za-z]{32})', sys.argv[1])
+    match = re.match('.*(darwin_[0-9A-Za-z]{12}_[0-9A-Za-z]{32})', sys.argv[1])
+
     filename = match.group(1)
 
     fullpath = "si_logs@theyard:/home/si_logs/translation/" + filename + "/" + logfile
 
+    # scp si_logs@theyard:/home/si_logs/translation/darwin_783e53fe4a60_81b952e11b7e50199c8d07a4649310c7/LOGlastrun_realtime from theyard to current dicrectory...
     sys.stdout.write("scp %s from theyard to current dicrectory...\n" % fullpath)
 
     subprocess.call(["scp", "-r", fullpath, "."])
