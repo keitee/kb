26ef0a7d460ba0bc856165bf21594947ad071ef0
Kit Park <kyoupark@cisco.com>
2018-07-05 18:31:47 +0100

    Thu  5 Jul 18:31:47 BST 2018 fh

--- code-cxx/t_ex_algo/t_algo.cpp
+++ code-cxx/t_ex_algo/t_algo.cpp
@@ -3443,9 +3443,360 @@ TEST(AlgoCountIdenticalPairs, 0629_01)
 // ={=========================================================================
 // algo-repairman
 
+typedef long long lint;
+
+int g_points[1000];
+int g_weights[1000];
+
+// the order of arg do not matter since it's abs. for example, abs(s-e) or abs(e-s)
+#define ABS_DISTANCE(s,e) (abs(g_points[e] - g_points[s]))
+
+lint sum_range(int start, int finish, lint start_distance = 0);
+lint sum_range_with_start(int start, int begin, int end, lint start_distance =0);
+
+// get weighted sum in (begin, end] from start offset. note that do not
+// include start in its sum.
+//
+// * loop condition and distance math
+// turns out have to have loop exit in the middle of loop as the orignial has.
+// Other attemtps like these cause probblems:
+//
+// for (int i = begin + direction; i != end ; i += direction)
+// cause to miss out the last in add so wrong result.
+//
+// for (int i = begin + direction; i != (end+1); i += direction)
+// cause to seg fault when do the left direction, direction is -1. 
+// 
+// the proper is:
+//
+// for (int i = begin + direction; ; i += direction)
+// {
+//   start_distance += ABS_DISTANCE(i-direction, i);
+//
+//   if (i == end)
+//    return weight_sum;
+// }
+//
+// Why? In order to support both direction and not inclding the start, use
+// clever way to do match. In particuler, the way `direction` is used.
+//
+// For example:
+//
+// sum_range(0,5)
+//  not include start, 0.
+//  direction = 1, i = 1, ABS(0,1), wi[1] 
+//  direction = 1, i = 2, ABS(1,2), wi[2] 
+//  direction = 1, i = 3, ABS(3,2), wi[3] 
+//  direction = 1, i = 4, ABS(4,3), wi[4] 
+//  direction = 1, i = 5, ABS(5,4), wi[5] 
+//  exit
+//
+// sum_range(5,0)
+//  not include start, 5.
+//  direction = -1, i = 4, ABS(5,4), wi[4] 
+//  direction = -1, i = 3, ABS(4,3), wi[3] 
+//  direction = -1, i = 2, ABS(3,2), wi[2] 
+//  direction = -1, i = 1, ABS(2,1), wi[1] 
+//  direction = -1, i = 0, ABS(1,0), wi[0] 
+//  exit
+//
+//
+// running:
+//
+//  i   0    1    2     3     4     5
+// wi   1    2    10    3     5     1
+// -----+----+-----+-----+-----+-----+---
+// xi   1    6    12    13    14    24
+//      s--------------------------->   244
+//      -----s
+//      ---------------------------->   329
+//      ----------s
+//      ---------------------------->   246
+//      ----------------s
+//      ---------------------------->   196
+//      ----------------------s
+//      ---------------------------->   88
+//      ...
+//
+// 86 is made of:
+//
+//                s-----*-----*
+//      *----*----
+//                            ------*
+//
+// where * denotes points used in sum.
+//
+//
+// # DURATION     TID     FUNCTION
+//             [   529] | search(0, 5) {
+// 
+//                          /* starts from 0 to 5 */
+//                          /* sum(start, begin, end, dis); */
+//    0.665 us [   529] |   sum(0, 5, 0, 5) = 244;
+// 
+//                          /* starts from 1 */
+//                          /* sum(begin, end, dis); */
+//             [   529] |   sum(1, 0, 5, 0) {
+//    0.443 us [   529] |     sum(1, 0, 0, 0) = 5;
+//    1.035 us [   529] |     sum(1, 5, 10, 5) = 329;
+//    1.219 ms [   529] |   } = 334; /* sum */
+// 
+//                          /* starts from 2 */
+//             [   529] |   sum(2, 0, 5, 0) {
+//    0.514 us [   529] |     sum(2, 0, 0, 0) = 23;
+//    0.499 us [   529] |     sum(2, 5, 22, 5) = 223;
+//    1.225 ms [   529] |   } = 246; /* sum */
+// 
+//                          /* starts from 3 */
+//             [   529] |   sum(3, 0, 5, 0) {
+//    0.554 us [   529] |     sum(3, 0, 0, 0) = 36;
+//    0.566 us [   529] |     sum(3, 5, 24, 5) = 160;
+//    1.078 ms [   529] |   } = 196; /* sum */
+// 
+//                          /* update minimum sum and save start point */
+// 
+//                          /* starts from 4 */
+//             [   529] |   sum(4, 0, 5, 0) {
+//    0.493 us [   529] |     sum(4, 0, 0, 0) = 52;
+//    0.431 us [   529] |     sum(4, 5, 26, 5) = 36;
+//  190.644 us [   529] |   } = 88; /* sum */
+// 
+//                          /* update minimum sum and save start point */
+// 
+//                          /* search [0,5] ends and there are updates so search again [4,0] */
+// 
+//             [   529] |   search(4, 0) {
+// 
+//                          /* starts from 4 to 0 */
+//    0.242 us [   529] |     sum(4, 0, 0, 0) = 52;
+// 
+//             [   529] |     sum(3, 4, 0, 0) {
+//    0.202 us [   529] |       sum(3, 4, 0, 4) = 5;
+//    0.394 us [   529] |       sum(3, 0, 2, 0) = 62;
+//  280.063 us [   529] |     } = 67; /* sum */
+// 
+//             [   529] |     sum(2, 4, 0, 0) {
+//    0.301 us [   529] |       sum(2, 4, 0, 4) = 13;
+//    0.220 us [   529] |       sum(2, 0, 4, 0) = 35;
+//   40.841 us [   529] |     } = 48; /* sum */
+// 
+//                          /* update minimum sum and save start point */
+// 
+//             [   529] |     sum(1, 4, 0, 0) {
+//    0.217 us [   529] |       sum(1, 4, 0, 4) = 121;
+//    0.211 us [   529] |       sum(1, 0, 16, 0) = 21;
+//   37.859 us [   529] |     } = 142; /* sum */
+// 
+//                          /* search [4,0] ends and there are updates so search again [2,4] */
+// 
+//             [   529] |     search(2, 4) {
+//    0.224 us [   529] |       sum(2, 4, 0, 4) = 13;
+// 
+//             [   529] |       sum(3, 2, 4, 0) {
+//    3.156 us [   529] |         sum(3, 2, 0, 2) = 10;
+//    0.221 us [   529] |         sum(3, 4, 2, 4) = 15;
+//   44.632 us [   529] |       } = 25; /* sum */
+// 
+//    0.208 us [   529] |       sum(2, 4, 0, 4) = 13;
+// 
+//                              /* search [2,4] ends
+//                                 add time[2, end 4], time +=2, time 2
+//                                 <13, 2> = sum(ind:2, e:4, time:0)
+//                              */
+//
+//                              /* means that start = 2 for minimul sum ! */ 
+//
+//   71.569 us [   529] |     } = 13; /* search */
+// 
+//                            /* still in search [4,0]
+//                               ind was 2, add time[2, start 4], time +=2, time 4 
+//                             */
+// 
+//    0.428 us [   529] |     sum(2, 0, 4, 0) = 35;
+// 
+//                            /* search [4,0] ends
+//                               add time[2, start 0], time +=11, time 15
+//                               <48, 15> = sum(ind:2, e:0, time:4)
+//                            */
+// 
+//  955.074 us [   529] |   } = 48; /* search */
+// 
+//                            /* still in search [0,5]
+//                               ind was 4, add time[4, start 0], time +=13, time 28 
+//                             */
+// 
+//    0.235 us [   529] |   sum(4, 5, 28, 5) = 38;
+// 
+//                            /* search [0,5] ends
+//                               add time[4, end 5], time +=10, time 38
+//                               <86, 38> sum(ind:4, e:5, time:28)
+//                            */
+// 
+//    6.085 ms [   529] | } = 86; /* search */
+// 
+//             [   529] | search(5, 0) {
+//    0.239 us [   529] |   sum(5, 0, 0, 0) = 262;
+//             [   529] |   sum(4, 5, 0, 0) {
+//    0.206 us [   529] |     sum(4, 5, 0, 5) = 10;
+//    0.223 us [   529] |     sum(4, 0, 20, 0) = 372;
+//   49.363 us [   529] |   } = 382; /* sum */
+//             [   529] |   sum(3, 5, 0, 0) {
+//    0.300 us [   529] |     sum(3, 5, 0, 5) = 16;
+//    0.295 us [   529] |     sum(3, 0, 22, 0) = 322;
+//   50.948 us [   529] |   } = 338; /* sum */
+//             [   529] |   sum(2, 5, 0, 0) {
+//    0.287 us [   529] |     sum(2, 5, 0, 5) = 25;
+//    0.305 us [   529] |     sum(2, 0, 24, 0) = 95;
+//   52.524 us [   529] |   } = 120; /* sum */
+//             [   529] |   sum(1, 5, 0, 0) {
+//    0.254 us [   529] |     sum(1, 5, 0, 5) = 139;
+//    0.249 us [   529] |     sum(1, 0, 36, 0) = 41;
+//   42.398 us [   529] |   } = 180; /* sum */
+//             [   529] |   search(2, 5) {
+//    0.223 us [   529] |     sum(2, 5, 0, 5) = 25;
+//             [   529] |     sum(3, 2, 5, 0) {
+//    0.202 us [   529] |       sum(3, 2, 0, 2) = 10;
+//    0.211 us [   529] |       sum(3, 5, 2, 5) = 28;
+//   37.587 us [   529] |     } = 38; /* sum */
+//             [   529] |     sum(4, 2, 5, 0) {
+//    0.279 us [   529] |       sum(4, 2, 0, 2) = 23;
+//    0.270 us [   529] |       sum(4, 5, 4, 5) = 14;
+//   56.792 us [   529] |     } = 37; /* sum */
+//    0.363 us [   529] |     sum(2, 5, 0, 5) = 25;
+//  657.999 us [   529] |   } = 25; /* search */
+//    0.286 us [   529] |   sum(2, 0, 24, 0) = 95;
+//  954.739 us [   529] | } = 120; /* search */
+//
+//  86 = min(86, 120)
+//
+// Things to note:
+//
+// 1. Always calculate distance from the start point.
+// 2. Do not count point which are already counted.
+// 3. The start point do not included in the sum.
+
+
+lint sum_range(int begin, int end, lint start_distance)
+{
+  if (begin == end)
+    return 0;
+
+  // forward or backward
+  int direction = ((end > begin) ? 1: -1);
+
+  lint weight_sum = 0;
+
+  for (int i = begin + direction; ; i += direction)
+  {
+    start_distance += ABS_DISTANCE(i-direction, i);
+    weight_sum += start_distance*(lint)g_weights[i];
+
+    if (i == end)
+      return weight_sum;
+  }
+
+  return weight_sum;
+}
+
+
+lint sum_range_with_start(int start, int begin, int end, lint start_distance)
+{
+	lint d1 = sum_range(start, begin, start_distance);
+
+	// why 2? since used twice to make a turn
+	lint d3 = sum_range(start, end, start_distance + 2*ABS_DISTANCE(begin, start));
+
+	return d1 + d3;
+}
+
+// search(start index, end index)
+pair<lint, lint> search(int begin, int end)
+{
+  lint current_sum = sum_range(begin, end, 0);
+  int current_start = begin;
+
+  if(begin == end)
+    return make_pair(0, 0);
+
+  int direction = ((end > begin)? 1:-1);
+
+  lint calculated_sum = 0;
+  lint calculated_distance = 0;
+
+  for (int start = begin + direction; start != end; start += direction)
+  {
+    lint range_sum  = sum_range_with_start(start, begin, end);
+
+    // found sum which is less than sum of [begin, end] and save start point.
+    // so `ind` is saved start which shows the minimum sum so far.
+    // 
+    // redundant check
+    // if (range_sum < current_sum && abs(start - begin) > abs(current_start - begin))
+
+    if (range_sum < current_sum)
+    {
+
+      current_sum = range_sum;
+      current_start = start;
+    }
+  }
+
+  // when found start point which has lesser sum since current_start do not
+  // change when there is no lesser sum found in the range.
+
+  if (current_start != begin)
+  {
+    pair<lint, lint> result = search(current_start, begin);	
+    calculated_sum += result.first;
+    calculated_distance += result.second + ABS_DISTANCE(current_start, begin);		
+  }
+
+  calculated_sum += sum_range(current_start, end, calculated_distance);
+  calculated_distance += ABS_DISTANCE(current_start, end);
+
+  return make_pair(calculated_sum, calculated_distance);
+}
+
+
 TEST(AlgoRapairman, 0704)
 {
+  {
+    vector<int> dcall{1, 6, 12, 13, 14, 24};
+    vector<int> wcall{1, 2, 10, 3, 5, 1};
+
+    for (size_t i = 0; i < dcall.size(); ++i)
+    {
+      g_points[i] = dcall[i];
+      g_weights[i] = wcall[i];
+    }
+
+    int N = dcall.size();
+
+    pair<lint, lint> res1 = search(0, N - 1);
+    pair<lint, lint> res2 = search(N - 1, 0);
+
+    EXPECT_THAT(min(res1.first, res2.first), 86);
+  }
+
+  {
+    vector<int> dcall{5, 34, 45, 49, 51, 52, 53, 56, 63, 81, 84, 88, 93, 99, 106};
+    vector<int> wcall{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+    for (size_t i = 0; i < dcall.size(); ++i)
+    {
+      g_points[i] = dcall[i];
+      g_weights[i] = wcall[i];
+    }
+
+    int N = dcall.size();
+
+    pair<lint, lint> res1 = search(0, N - 1);
+    pair<lint, lint> res2 = search(N - 1, 0);
+
+    EXPECT_THAT(min(res1.first, res2.first), 630);
+  }
 }
+
 
 // ={=========================================================================
 // algo-atoi
