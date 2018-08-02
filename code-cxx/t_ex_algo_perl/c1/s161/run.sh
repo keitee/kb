3eb46881c2f80d8e65382fe4f42a9c1247190a8f
Kit Park <kyoupark@cisco.com>
2018-08-02 15:06:59 +0100

    Thu  2 Aug 15:06:59 BST 2018 fh

--- code-cxx/t_ex_algo_perl/c1/s161/run.sh
+++ code-cxx/t_ex_algo_perl/c1/s161/run.sh
@@ -1,6 +1,12 @@
 #/bim/bash
 
+# 10,000,000
 time ./s161-qsort_test < ../numbers.in
 time ./s161-stl_test < ../numbers.in
 time ./s161-bit_test < ../numbers.in
+
+# 1,000,000
+time ./s161-qsort_test < ../numbers2.in
+time ./s161-stl_test < ../numbers2.in
+time ./s161-bit_test < ../numbers2.in
 
