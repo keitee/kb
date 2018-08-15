559db830e5763ef7a0f46d69ffc317740fd135d5
Kit Park <keiteepark@gmail.com>
2018-08-15 09:18:48 +0100

    Wed 15 Aug 09:18:48 BST 2018 kt-office-debian

---
+++ code-qt/notepad/notepad.cpp
@@ -0,0 +1,108 @@
+#include <QTextStream>
+#include <QMessageBox>
+#include <QFileDialog>
+
+#include "notepad.h"
+#include "ui_notepad.h"
+
+Notepad::Notepad(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
+
+void Notepad::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void Notepad::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file..");
+    QFile file(fileName);
+
+    // save
+    currentFile = fileName;
+
+    if (!file.open(QIODevice::ReadOnly| QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot open fle: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void Notepad::on_actionSave_triggered()
+{
+    QString fileName;
+
+    if (currentFile.isEmpty())
+    {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    }
+    else
+    {
+        fileName = currentFile;
+    }
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void Notepad::on_actionSave_as_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+
+    if (!file.open(QIODevice::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void Notepad::on_actionExit_triggered()
+{
+    QCoreApplication::quit();
+}
+
+void Notepad::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void Notepad::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
---
+++ code-qt/notepad/notepad.h
@@ -0,0 +1,32 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class Notepad;
+}
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Notepad(QWidget *parent = 0);
+    ~Notepad();
+
+private slots:
+    void on_actionNew_triggered();
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionSave_as_triggered();
+    void on_actionExit_triggered();
+    void on_actionCopy_triggered();
+    void on_actionPaste_triggered();
+
+private:
+    Ui::Notepad *ui;
+    QString currentFile;
+};
+
+#endif // NOTEPAD_H
