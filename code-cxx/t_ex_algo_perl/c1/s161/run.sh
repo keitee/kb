412da86e1cc1884d7408416795b8b81ddeece82d
Kit Park <keiteepark@gmail.com>
2018-08-28 08:16:10 +0100

    Tue 28 Aug 08:16:10 BST 2018 kit-debian64

--- code-cxx/t_ex_algo_perl/c1/s161/run.sh
+++ code-cxx/t_ex_algo_perl/c1/s161/run.sh
@@ -6,7 +6,7 @@ time ./s161-stl_test < ../numbers.in
 time ./s161-bit_test < ../numbers.in
 time ./s161-stl-sort_test < ../numbers.in
 time ./s161-stl-sort-array_test < ../numbers.in
-time ./s161-isort_test < ../numbers.in
+# time ./s161-isort_test < ../numbers.in
 time ./s161-qsort-own_test < ../numbers.in
 
 # 1,000,000 ../numbers2.in
@@ -15,5 +15,5 @@ time ./s161-stl_test < ../numbers2.in
 time ./s161-bit_test < ../numbers2.in
 time ./s161-stl-sort_test < ../numbers2.in
 time ./s161-stl-sort-array_test < ../numbers2.in
-time ./s161-isort_test < ../numbers2.in
+# time ./s161-isort_test < ../numbers2.in
 time ./s161-qsort-own_test < ../numbers2.in
