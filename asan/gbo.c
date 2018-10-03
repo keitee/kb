3c077eb8f00c2f8620487213f784d664afe9f2ea
Kit Park <keiteepark@gmail.com>
2018-10-03 10:53:22 +0100

    Wed  3 Oct 10:53:22 BST 2018 kit-debian64

--- asan/gbo.c
+++ asan/gbo.c
@@ -6,6 +6,60 @@
 global-buffer-overflow
 gcc -fsanitize=address testmain.c -o out_asn
 
+32 bits
+
+kyoupark@kt-office-debian:~/git/kb/asan$ ./gbo_host_asan
+main: begin.
+main: this global buffer overflow..
+main:
+=================================================================
+==28181==ERROR: AddressSanitizer: global-buffer-overflow on address 0x00447060 at pc 0x0044593a bp 0xbf7ffb18 sp 0xbf7ffb0c
+READ of size 8 at 0x00447060 thread T0
+    #0 0x445939 in call_gbo /home/kyoupark/git/kb/asan/gbo.c:62
+    #1 0x4459f2 in main /home/kyoupark/git/kb/asan/gbo.c:72
+    #2 0xb7042285 in __libc_start_main (/lib/i386-linux-gnu/libc.so.6+0x18285)
+    #3 0x445760  (/home/kyoupark/git/kb/asan/gbo_host_asan+0x760)
+
+0x00447066 is located 0 bytes to the right of global variable 'gclientname' defined in 'gbo.c:57:13' (0x447060) of size 6
+  'gclientname' is ascii string '_DLRH'
+SUMMARY: AddressSanitizer: global-buffer-overflow /home/kyoupark/git/kb/asan/gbo.c:62 in call_gbo
+Shadow bytes around the buggy address:
+  0x20088db0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088dc0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088dd0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088de0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088df0: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+=>0x20088e00: 00 00 00 00 00 00 00 00 00 00 00 00[06]f9 f9 f9
+  0x20088e10: f9 f9 f9 f9 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088e20: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088e30: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088e40: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+  0x20088e50: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+Shadow byte legend (one shadow byte represents 8 application bytes):
+  Addressable:           00
+  Partially addressable: 01 02 03 04 05 06 07
+  Heap left redzone:       fa
+  Heap right redzone:      fb
+  Freed heap region:       fd
+  Stack left redzone:      f1
+  Stack mid redzone:       f2
+  Stack right redzone:     f3
+  Stack partial redzone:   f4
+  Stack after return:      f5
+  Stack use after scope:   f8
+  Global redzone:          f9
+  Global init order:       f6
+  Poisoned by user:        f7
+  Container overflow:      fc
+  Array cookie:            ac
+  Intra object redzone:    bb
+  ASan internal:           fe
+  Left alloca redzone:     ca
+  Right alloca redzone:    cb
+==28181==ABORTING
+
+64 bits
+
 kyoupark@kit-debian64:~/git/kb/asan$ ./gbo_host_asan
 main: begin.
 main: this global buffer overflow..
