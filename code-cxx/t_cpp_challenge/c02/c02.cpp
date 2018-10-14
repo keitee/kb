fa7c6f676971a93b80a434709939dd9510d9dfb8
Kit Park <keiteepark@gmail.com>
2018-10-14 20:03:39 +0100

    from home debian

--- code-cxx/t_cpp_challenge/c02/c02.cpp
+++ code-cxx/t_cpp_challenge/c02/c02.cpp
@@ -22,25 +22,61 @@ should be formatted to an output stream.
 
 */
 
-TEST(CH3, IPV4)
+class ipv4
 {
-  const auto inputs{ 
-    make_pair(3, 12), 
-    make_pair(2, 34), 
-    make_pair(13, 245)
-  };
+  friend ostream &operator<<(ostream &os, ipv4 ip);
+  // friend ostream &operator>>(ostream &is, ipv4 ip);
+  friend istream &operator>>(istream &is, ipv4 ip);
 
-  unsigned int r1{}, r2{};
-
-  for (const auto e : inputs)
+  public:
+  ipv4(const int value1=0, const int value2=0, 
+      const int value3=0, const int value4=0)
+    : value1_(value1), value2_(value2), value3_(value3), value4_(value4)
   {
-    r1 = gcd_recursive(e.first, e.second);
-    r2 = gcd_iteratitive(e.first, e.second);
-
-    // cout << "r1: " << r1 << ", r2: " << r2 << endl;
-
-    EXPECT_EQ(r1, r2);
+    cout << "ipv4:ctor" << endl;
   }
+
+  private:
+  int value1_, value2_, value3_, value4_;
+};
+
+
+ostream &operator<<(ostream &os, const ipv4 ip)
+{
+  // convert them into string
+  stringstream address{};
+  address << "ip address is: " << 
+    ip.value1_ << "." << ip.value2_ << "." << ip.value3_ << "." << ip.value4_;
+  cout << address.str();
+  return os;
+}
+
+// this causes infinite loop on cin and eventually seg fault because
+// ip's ctor is called on and on.
+//
+// 1. have tried to use only one input. not?
+// istream &operator>>(istream &is, const ipv4 ip)
+
+// istream &operator>>(istream &is, ipv4 ip)
+istream &operator>>(istream &is, const ipv4 ip)
+{
+  cin >> ip.value1_;
+  // cin >> ip.value1_ >> ip.value2_ >> ip.value3_ >> ip.value4_;
+  return is;
+}
+
+
+TEST(LanguageFeatures, IPV4)
+{
+  ipv4 address(168, 192, 0, 1);   
+  std::cout << address << std::endl;   
+
+  ipv4 ip;   
+  std::cout << ip << std::endl;   
+  std::cin >> ip;   
+
+  if(!std::cin.fail())      
+    std::cout << ip << std::endl;
 }
 
 
