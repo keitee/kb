6fc33ce8ceeacd008601fa7410e1647fd0ac728f
Kit Park <keiteepark@gmail.com>
2018-12-21 13:30:44 +0000

    Fri 21 Dec 13:30:43 GMT 2018 kit-debian64

--- code-cxx/ccon/ccon.cpp
+++ code-cxx/ccon/ccon.cpp
@@ -15,8 +15,14 @@ using namespace std;
 using namespace std::placeholders;
 using namespace testing;
 
+// CXXSLR 18.1 The High-Level Interface: async() and Futures
+//
+// To visualize what happens, we simulate the complex processings in func1() and
+// func2() by calling doSomething(),
+
 int doSomething (char c)
 {
+  // *cxx-random*
   default_random_engine dre(c);
   uniform_int_distribution<int> id(10, 1000);
 
@@ -25,6 +31,7 @@ int doSomething (char c)
     this_thread::sleep_for(chrono::milliseconds( id(dre) ));
     cout.put(c).flush();
   }
+  cout.put('\n');
 
   return c;
 }
@@ -59,7 +66,326 @@ TEST(CConThread, TwoAndLambda)
 
 
 // ={=========================================================================
-// cxx-lock
+// cxx-async
+
+namespace cxx_async {
+
+  int sum{0};
+
+  int find_the_answer()
+  {
+    this_thread::sleep_for(chrono::milliseconds(100));
+    return sum + 90;
+  }
+
+  void do_other_stuff()
+  {
+    this_thread::sleep_for(chrono::seconds(20));
+    sum = 10;
+  }
+
+} // namespace
+
+// expect that do_other_stuff() finishes first
+
+TEST(CConAsync, Async)
+{
+  using namespace cxx_async;
+
+  std::future<int> the_answer = std::async(find_the_answer);
+  do_other_stuff();
+  EXPECT_THAT(the_answer.get(), 100);
+}
+
+namespace cxx_aync_return {
+
+  void use_reference(int &value)
+  {
+    value += 200;
+  }
+
+  void use_value(int value)
+  {
+    value += 200;
+  }
+
+} // namespace
+
+// show how return value from thread via reference
+
+TEST(CConAsync, ReturnViaReference)
+{
+  using namespace cxx_aync_return;
+
+  // value
+  {
+    int value{1};
+
+    std::thread t(use_value, value); 
+    t.join();
+
+    EXPECT_THAT(value, 1);
+  }
+
+  {
+    int value{1};
+
+    // In file included from /usr/include/c++/4.9/thread:39:0,
+    //                  from ccon.cpp:2:
+    // /usr/include/c++/4.9/functional: In instantiation of ‘struct std::_Bind_simple<void (*(int))(int&)>’:
+    // /usr/include/c++/4.9/thread:140:47:   required from ‘std::thread::thread(_Callable&&, _Args&& ...) [with _Callable = void (&)(int&); _Args = {int&}]’
+    // ccon.cpp:418:36:   required from here
+    // /usr/include/c++/4.9/functional:1665:61: error: no type named ‘type’ in ‘class std::result_of<void (*(int))(int&)>’
+    //        typedef typename result_of<_Callable(_Args...)>::type result_type;
+    //                                                              ^
+    // /usr/include/c++/4.9/functional:1695:9: error: no type named ‘type’ in ‘class std::result_of<void (*(int))(int&)>’
+    //          _M_invoke(_Index_tuple<_Indices...>)
+    //          ^
+    // makefile:58: recipe for target 'ccon.o' failed
+    // make: *** [ccon.o] Error 1
+    //
+    // std::thread t(UseReference, value);  
+
+    // *cxx-ref*
+    std::thread t(use_reference, std::ref(value));  
+    t.join();
+
+    EXPECT_THAT(value, 201);
+  }
+
+  {
+    int value{1};
+
+    std::thread t([&]{use_reference(value);}); 
+    t.join();
+
+    EXPECT_THAT(value, 201);
+  }
+}
+
+
+// Note, finally, that the object passed to async() may be any type of a
+// callable object:
+//
+// A call of async() does not guarantee that the passed functionality gets
+// started and finished.
+// 
+// It is necessary to ensure that sooner or later, the passed functionality gets
+// called. Note that I wrote that async() `tries` to start the passed
+// functionality. If this didn’t happen we need the future object to force a
+// start when we need the result or want to ensure that the functionality was
+// performed. Thus, you need the future object even if you are not interested in
+// the outcome of a functionality started in the background.
+//
+// Accordingly, two kinds of outputs are possible for this program. 
+
+// cxx-async with no option
+//
+// If async() couldn’t start func1(), it will run after func2(), when get() gets
+// called, so that the program will have the following output:
+//
+// starting func1() in background and func2() in foreground:
+// ++++++++++..........
+// result of func1()+func2(): 89
+
+
+TEST(CConAsync, WithDefault)
+{
+  future<int> result1(std::async([]{return doSomething('.');}));
+
+  int result2 = doSomething('+');
+
+  int result = result1.get() + result2;
+
+  EXPECT_THAT(result, 89);
+}
+
+
+// cxx-async with async option
+//
+// If async() could successfully start func1(), the output might be something
+// like the following:
+//
+// starting func1() in background and func2() in foreground:
+// +..+..+...+..+.+++++
+// result of func1()+func2(): 89
+
+
+// Using Launch Policies
+
+// You can force async() to not defer the passed functionality, by explicitly
+// passing a launch policy directing async() that it should definitely start the
+// passed functionality asynchronously the moment it is called:
+
+TEST(CConAsync, WithAsync)
+{
+  future<int> result1(std::async(launch::async, []{return doSomething('.');}));
+
+  int result2 = doSomething('+');
+
+  int result = result1.get() + result2;
+
+  EXPECT_THAT(result, 89);
+}
+
+// So, based on this first example, we can define a general way to make a
+// program faster: You can modify the program so that it might benefit from
+// parallelization, if the underlying platform supports it, but still works as
+// before on single-threaded environments.
+
+
+// Note also that you have to ensure that you ask for the result of a
+// functionality started with async() no earlier than necessary. For example,
+// the following “optimization” is probably not what you want:
+//
+// std::future<int> result1(std::async(func1));
+// int result = func2() + result1.get(); // might call func2() after func1() ends
+//
+// Because the evaluation order on the right side of the second statement is
+// unspecified, result1.get() might be called before func2() so that you have
+// sequential processing again.
+
+
+// If you don’t assign the result of std::async(std::launch::async,...)
+// anywhere, the caller will block until the passed functionality has finished,
+// which would mean that this is nothing but a synchronous call.
+// 
+// ..........  
+// ++++++++++
+
+TEST(CConAsync, WithAsyncButSequential)
+{
+  // future<int> result1(std::async(launch::async, []{return doSomething('.');}));
+  std::async(launch::async, []{return doSomething('.');});
+
+  doSomething('+');
+}
+
+
+// ={=========================================================================
+// cxx-async with async option
+
+// starting 2 operations asynchronously
+// 
+// ..........++++++++++
+// done
+
+TEST(CConAsync, UseAsyncX)
+{
+  cout << "starting 2 operations asynchronously" << endl;
+
+  // start two loops in the background printing characters . or +
+  auto f1 = async([]{ doSomething('.'); });
+  auto f2 = async([]{ doSomething('+'); });
+
+  // if at least one of the background tasks is running
+  if (f1.wait_for(chrono::seconds(0)) != future_status::deferred ||
+      f2.wait_for(chrono::seconds(0)) != future_status::deferred) 
+  {
+    cout << "\ndeffered" << endl;
+
+    // poll until at least one of the loops finished
+    while (f1.wait_for(chrono::seconds(0)) != future_status::ready &&
+        f2.wait_for(chrono::seconds(0)) != future_status::ready) 
+    {
+      //...;
+      cout << "\nyield" << endl;
+      this_thread::yield();  // hint to reschedule to the next thread
+    }
+  }
+  cout.put('\n').flush();
+
+  // wait for all loops to be finished and process any exception
+  try {
+    f1.get();
+    f2.get();
+  }
+  catch (const exception& e) {
+    cout << "\nEXCEPTION: " << e.what() << endl;
+  }
+  cout << "\ndone" << endl;
+}
+
+
+// ={=========================================================================
+// cxx-lock-mutex cxx-mutex
+
+// CXXSLR 18.5 A First Complete Example for Using a Mutex and a Lock
+
+// see the effect of lock
+//
+// [ RUN      ] CConMutex.LockGuard
+// Hello from a main thread
+// Hello from a second thread
+// Hello from a first thread
+// [       OK ] CConMutex.LockGuard (1520 ms)
+// [ RUN      ] CConMutex.NoLockGuard
+// HHHeeellllllooo   fffrrrooommm   aaa   msfaeiicrnos nttd h trthehrared
+// eaad
+// d
+// [       OK ] CConMutex.NoLockGuard (528 ms)
+
+namespace cxx_mutex
+{
+  std::mutex print_mutex;
+
+  void print(std::string const& s)
+  {
+    std::lock_guard<std::mutex> l(print_mutex);
+
+    for (char c : s)
+    {
+      this_thread::sleep_for(chrono::milliseconds(20));
+      std::cout.put(c);
+    }
+    std::cout << std::endl;
+  }
+} // cxx_mutex
+
+TEST(CConLock, LockGuard)
+{
+  using namespace cxx_mutex;
+
+  auto f1 = std::async(std::launch::async,
+      print, "Hello from a first thread");
+
+  auto f2 = std::async(std::launch::async,
+      print, "Hello from a second thread");
+
+  print("Hello from a main thread");
+}
+
+namespace cxx_mutex_no_lock
+{
+  std::mutex print_mutex;
+
+  void print(std::string const& s)
+  {
+    // std::lock_guard<std::mutex> l(print_mutex);
+
+    for (char c : s)
+    {
+      this_thread::sleep_for(chrono::milliseconds(20));
+      std::cout.put(c);
+    }
+    std::cout << std::endl;
+  }
+} // cxx_mutex
+
+TEST(CConLock, NoLockGuard)
+{
+  using namespace cxx_mutex_no_lock;
+
+  auto f1 = std::async(std::launch::async,
+      print, "Hello from a first thread");
+
+  auto f2 = std::async(std::launch::async,
+      print, "Hello from a second thread");
+
+  print("Hello from a main thread");
+}
+
+// found that sometimes sum is 45 and not sure why?
 
 namespace cxx_lock
 {
@@ -84,26 +410,22 @@ namespace cxx_lock
         std::mutex mutex_;
     };
 
-    void do_something(int& start, int& sum, std::mutex& m)
+  void do_something(int& start, int& sum, std::mutex& m)
+  {
+    while (start < 10)
     {
-      default_random_engine engine(start);  // cxx-random-seed
-      uniform_int_distribution<int> dist(10, 1000);
-
-      while (start < 10)
-      {
-        {
-          std::lock_guard<std::mutex> lock(m);
-          // this_thread::sleep_for(chrono::milliseconds(dist(engine)));
-          cout << "start: " << start << ", sum: " << sum << endl;
-          sum += start;
-          ++start;
-        }
-      }
+      std::lock_guard<std::mutex> lock(m);
+      // this_thread::sleep_for(chrono::milliseconds(dist(engine)));
+      cout << ", start: " << start << ", sum: " << sum << endl;
+      // cout << msg << ", start: " << start << ", sum: " << sum << endl;
+      sum += start;
+      ++start;
     }
+  }
 
 } // namespace
 
-TEST(CConThread, LockGuard)
+TEST(CConLock, X)
 {
   using namespace cxx_lock;
 
@@ -113,9 +435,21 @@ TEST(CConThread, LockGuard)
   int sum{0};
   int start{1};
 
+  // OK
+  // std::thread t1([&]{ do_something("t1", start, sum, mutex_); });
+  // std::thread t2([&]{ do_something("t2", start, sum, mutex_); });
+  // std::thread t3([&]{ do_something("t3", start, sum, mutex_); });
+
+  // FAIL or OK
   std::thread t1([&]{ do_something(start, sum, mutex_); });
   std::thread t2([&]{ do_something(start, sum, mutex_); });
   std::thread t3([&]{ do_something(start, sum, mutex_); });
+
+  // OK
+  // std::thread t1([&]{ do_something(std::ref(start), std::ref(sum), std::ref(mutex_)); });
+  // std::thread t2([&]{ do_something(std::ref(start), std::ref(sum), std::ref(mutex_)); });
+  // std::thread t3([&]{ do_something(std::ref(start), std::ref(sum), std::ref(mutex_)); });
+
   t1.join();
   t2.join();
   t3.join();
@@ -445,171 +779,6 @@ TEST(CconThreadTest, UseThreadSafeLookupTable)
   cout << "searched value : " << tslt.value_for("three") << endl;
 }
 
-
-// ={=========================================================================
-// cxx-async,
-//
-// ends do_other_stuff
-// ends find_the_answer
-// the answer is 100
-
-int find_the_answer()
-{
-    this_thread::sleep_for(chrono::milliseconds(100));
-    std::cout << "ends find_the_answer" << std::endl;
-    return 100;
-}
-
-void do_other_stuff()
-{
-    this_thread::sleep_for(chrono::seconds(20));
-    std::cout << "ends do_other_stuff" << std::endl;
-}
-
-TEST(CconThreadTest, UseAsync)
-{
-    std::future<int> the_answer = std::async( find_the_answer );
-    do_other_stuff();
-    std::cout << "the answer is " << the_answer.get() << std::endl;
-}
-
-void UseReference(int &value)
-{
-  std::cout << "input   value: " << value << std::endl;
-  value += 200;
-  std::cout << "changed value: " << value << std::endl;
-}
-
-// [ RUN      ] CconThreadTest.CompareThreadAndAsync
-// input   value: 1
-// changed value: 201
-// final value: 201
-// [       OK ] CconThreadTest.CompareThreadAndAsync (1 ms)
-
-TEST(CconThreadTest, UseAsyncWithReference)
-{
-  int value{1};
-
-  // In file included from /usr/include/c++/4.9/thread:39:0,
-  //                  from ccon.cpp:2:
-  // /usr/include/c++/4.9/functional: In instantiation of ‘struct std::_Bind_simple<void (*(int))(int&)>’:
-  // /usr/include/c++/4.9/thread:140:47:   required from ‘std::thread::thread(_Callable&&, _Args&& ...) [with _Callable = void (&)(int&); _Args = {int&}]’
-  // ccon.cpp:418:36:   required from here
-  // /usr/include/c++/4.9/functional:1665:61: error: no type named ‘type’ in ‘class std::result_of<void (*(int))(int&)>’
-  //        typedef typename result_of<_Callable(_Args...)>::type result_type;
-  //                                                              ^
-  // /usr/include/c++/4.9/functional:1695:9: error: no type named ‘type’ in ‘class std::result_of<void (*(int))(int&)>’
-  //          _M_invoke(_Index_tuple<_Indices...>)
-  //          ^
-  // makefile:58: recipe for target 'ccon.o' failed
-  // make: *** [ccon.o] Error 1
-  //
-  // std::thread t(UseReference, value);  
-
-  std::thread t(UseReference, std::ref(value));  
-  t.join();
-  std::cout << "final value: " << value << std::endl;
-}
-
-
-// ={=========================================================================
-// cxx-async with no option
-
-// starting func1() in background and func2() in foreground:
-// ++++++++++..........
-// result of func1()+func2(): 89
-
-
-int func1()
-{
-    return doSomething('.');
-}
-
-int func2()
-{
-    return doSomething('+');
-}
-
-TEST(CconThreadTest, UseAsyncWithDefault)
-{
-    cout << "starting func1() in background"
-        << " and func2() in foreground:" << endl;
-
-    future<int> result1(std::async(func1));
-
-    int result2 = func2();
-
-    int result = result1.get() + result2;
-
-    cout << "\nresult of func1()+func2(): " << result << endl;
-}
-
-
-// ={=========================================================================
-// cxx-async with async option
-
-// starting func1() in background and func2() in foreground:
-// +..+..+...+..+.+++++
-// result of func1()+func2(): 89
-
-TEST(CconThreadTest, UseAsyncWithAsync)
-{
-    cout << "starting func1() in background"
-        << " and func2() in foreground:" << endl;
-
-    future<int> result1(std::async(launch::async, func1));
-
-    int result2 = func2();
-
-    int result = result1.get() + result2;
-
-    cout << "\nresult of func1()+func2(): " << result << endl;
-}
-
-
-// ={=========================================================================
-// cxx-async with async option
-
-// starting 2 operations asynchronously
-// 
-// ..........++++++++++
-// done
-
-TEST(CconThreadTest, UseAsyncX)
-{
-    cout << "starting 2 operations asynchronously" << endl;
-
-    // start two loops in the background printing characters . or +
-    auto f1 = async([]{ doSomething('.'); });
-    auto f2 = async([]{ doSomething('+'); });
-
-    // if at least one of the background tasks is running
-    if (f1.wait_for(chrono::seconds(0)) != future_status::deferred ||
-        f2.wait_for(chrono::seconds(0)) != future_status::deferred) 
-    {
-        cout << "\ndeffered" << endl;
-
-        // poll until at least one of the loops finished
-        while (f1.wait_for(chrono::seconds(0)) != future_status::ready &&
-               f2.wait_for(chrono::seconds(0)) != future_status::ready) 
-        {
-            //...;
-            cout << "\nyield" << endl;
-            this_thread::yield();  // hint to reschedule to the next thread
-        }
-    }
-    cout.put('\n').flush();
-
-    // wait for all loops to be finished and process any exception
-    try {
-        f1.get();
-        f2.get();
-    }
-    catch (const exception& e) {
-        cout << "\nEXCEPTION: " << e.what() << endl;
-    }
-    cout << "\ndone" << endl;
-}
 
 
 // ={=========================================================================
