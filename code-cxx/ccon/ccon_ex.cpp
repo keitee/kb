f5f255f5adf3c09daa6123a6b09718aeb5a1025b
Kit Park <keiteepark@gmail.com>
2018-12-21 14:17:09 +0000

    Fri 21 Dec 14:17:09 GMT 2018 kit-debian64

--- code-cxx/ccon/ccon_ex.cpp
+++ code-cxx/ccon/ccon_ex.cpp
@@ -15,26 +15,78 @@ using namespace std;
 using namespace std::placeholders;
 using namespace testing;
 
+
 // ={=========================================================================
-// cxx-ex
+// cxx-condition
 
 // class locked_queue {
 //  void push(T &);
 //  T pop();
 // };
 
-template <typename T>
-class locked_queue {
-  public:
-    void push(T const& item)
-    {
-    }
+namespace cxx_condition
+{
+  template <typename T>
+    class locked_queue {
+      public:
+        void push(T const& item)
+        {
+          std::lock_guard<std::mutex> lock(m_);
+          queue_.push(item);
 
-  private:
-    std::queue<T> queue_;
-    std::mutex lock_;
-    std::condition_variable cond_;
-};
+          // as soon as there are items in queue
+          if (queue_.size())
+            cond_.notify_one();
+        }
+
+        T pop() 
+        {
+          // to use with cxx-condition
+          std::unique_lock<std::mutex> lock(m_);
+
+          while (queue_.empty())
+            cond_.wait(m_);
+
+          T item = queue_.front();
+          queue_.pop();
+          return item;
+        }
+
+      private:
+        std::queue<T> queue_;
+        std::mutex m_;
+        std::condition_variable cond_;
+    };
+
+  void producer(locked_queue<int> &q)
+  {
+    for (int i = 0; i < 10; ++i)
+    {
+      ++value;
+    }
+  }
+
+  void consumer(locked_queue<int> &q)
+  {
+    for (int i = 0; i < 10; ++i)
+    {
+      ++value;
+    }
+  }
+
+} // namesapce
+
+TEST(CConCondition, ProducerAndConsumer)
+{
+  using namespace cxx_condition;
+
+  locked_queue<int> q;
+
+  std::thread c1(&consumer, &q);
+  std::thread c2(&consumer, &q);
+  std::thread c3(&consumer, &q);
+}
+
 
 // ={=========================================================================
 int main(int argc, char** argv)
