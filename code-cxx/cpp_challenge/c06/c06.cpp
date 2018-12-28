2d6d51de531da4dcbb1fbb993737d8d4109fbb28
Kit Park <keiteepark@gmail.com>
2018-12-28 08:09:24 +0000

    Fri 28 Dec 08:09:23 GMT 2018 kit-debian64

--- code-cxx/cpp_challenge/c06/c06.cpp
+++ code-cxx/cpp_challenge/c06/c06.cpp
@@ -688,7 +688,29 @@ of input and output examples:
 44 7555 123456  => +447555123456
 7555 123456     => +447555123456
 
+{
+   std::vector<std::string> numbers{
+      "07555 123456",
+      "07555123456",
+      "+44 7555 123456",
+      "44 7555 123456",
+      "7555 123456"
+   };
+
+   normalize_phone_numbers(numbers, "44");
+
+   for (auto const & number : numbers)
+   {
+      std::cout << number << std::endl;
+   }
+}
+
 */
+
+namespace U51_2018_12_28
+{
+  void 
+} // namespace
 
 
 // ={=========================================================================
