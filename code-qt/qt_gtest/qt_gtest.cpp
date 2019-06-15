e75f0ec15ae86dcb687490bb4440fa8ad2b1b7d1
Kit Park <kit.park@sky.uk>
2019-06-15 03:30:41 +0100

    Sat 15 Jun 03:30:41 BST 2019 kit-hdebi

--- code-qt/qt_gtest/qt_gtest.cpp
+++ code-qt/qt_gtest/qt_gtest.cpp
@@ -1,16 +1,64 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <set>
+#include <memory>
+#include <exception>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
 #include <QList>
 #include <QString>
-// #include <QMessageLogger>
 #include <QDebug>
 #include <QLoggingCategory>
 
 Q_LOGGING_CATEGORY(lcEditor, "qtc.editor");
 
-int main(int argc, char *argv[])
-{
-  // QCoreApplication a(argc, argv);
+using namespace std;
 
+// ={=========================================================================
+// -expect
+
+TEST(Qt, OrderOfArg)
+{
+  int value{100};
+
+  EXPECT_THAT(value, 100);
+  EXPECT_THAT(100, value);
+
+  // // t_ex_mock.cpp:262: Failure
+  // // Value of: value
+  // // Expected: is equal to 101
+  // //   Actual: 100 (of type int)
+  //
+  // EXPECT_THAT(value, 101);
+
+  // //
+  // // t_ex_mock.cpp:263: Failure
+  // // Value of: 101
+  // // Expected: is equal to 100
+  // //   Actual: 101 (of type int)
+  // EXPECT_THAT(101, value);
+}
+
+// to see which makes better messages
+//
+// [ RUN      ] Gtest.ExpectOrEq
+// gtest.cpp:283: Failure
+// Value of: 101
+// Expected: is equal to 100
+//   Actual: 101 (of type int)
+//
+// gtest.cpp:284: Failure
+// Expected equality of these values:
+//   101
+//   value
+//     Which is: 100
+// [  FAILED  ] Gtest.ExpectOrEq (0 ms)
+// [----------] 2 tests from Gtest (0 ms total)
+
+TEST(Qt, ExpectOrEq)
+{
+  // QCoreApplication a(argc, argv); 
   // print QList coll
   {
     QList<QString> coll;
@@ -69,6 +117,14 @@ int main(int argc, char *argv[])
     qCCritical(lcEditor) << "Hello critical category logging";
     qCDebug(lcEditor, "%s", "Hello debug category logging");
   }
-
   // return a.exec();
+}
+
+int main(int argc, char **argv)
+{
+  // Since Google Mock depends on Google Test, InitGoogleMock() is
+  // also responsible for initializing Google Test.  Therefore there's
+  // no need for calling testing::InitGoogleTest() separately.
+  testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
 }
