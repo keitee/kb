f00218dfd535be4de5ce5e1712d08b4f93d5b76c
Kit Park <kit.park@sky.uk>
2019-06-21 23:49:20 +0100

    Fri 21 Jun 23:49:20 BST 2019 kit-hdebi

--- code-qt/ctut-07/build/CMakeFiles/ctut-07_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "ctut-07_automoc.cpp"
-  "CMakeFiles/ctut-07_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ctut-07_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
