4164c0ea81850ce7aaa3235db55721567f48e7fa
Kit Park <kit.park@sky.uk>
2019-06-26 18:30:30 +0100

    Wed 26 Jun 18:30:30 BST 2019 keitee-mint

---
+++ bin/bldtag_sky_ai.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    # execute only if run as a script
+
+    # cleanup files
+    subprocess.call(["rm", "-f", "G* tags flist.out"])
+
+    # dirs to include
+    dirs = [
+        '/home/keitee/mw/ethan-as-source/xSubmoduleAppInfrastructure/AppInfrastructure/Common',
+        '/home/keitee/mw/ethan-as-source/xSubmoduleAppInfrastructure/AppInfrastructure/BluetoothRcu'
+        ]
+
+    listfile = 'flist.out'
+    logfile = open(listfile, 'w+')
+
+    # dirs to exclude
+    # find {to be replaced} -type d \( -path '*/build' -o -path '*/mock' \) -prune -o -print
+    # subprocess.call(["find", ".", "-type d \( -path '*/build' -o -path '*/mock' \)", "-prune", "-o", "-print"])
+    command = ["find", ".", "-type", "d", 
+        "(", 
+        "-path",  "*/build", "-o", 
+        # note on this end entry which is differnt from the above
+        "-path", "*/test",
+        ")", "-prune", "-o", "-type", "f", "-print"]
+
+    for e in dirs:
+      command[1] = e
+      subprocess.call(command, stdout=logfile)
+
+    logfile.close()
+
+    sys.stdout.write("building ctags for %s ...\n" % listfile)
+    subprocess.call(["ctags", "-L", listfile])
+
+    sys.stdout.write("building gtags for %s ...\n" % listfile)
+    subprocess.call(["gtags", "--skip-unreadable", "-f", listfile])
+
+    sys.stdout.write("done\n")
