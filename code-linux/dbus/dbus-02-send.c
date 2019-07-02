d7c31085a7fff51ef1b70df6eceb3dd2b8f29404
Kit Park <kit.park@sky.uk>
2019-07-02 23:53:33 +0100

    Tue  2 Jul 23:53:32 BST 2019 kit-hdebi

---
+++ code-linux/dbus/dbus-02-send.c
@@ -0,0 +1,121 @@
+/* 
+https://linoxide.com/how-tos/d-bus-ipc-mechanism-linux/
+
+Example 2: Connecting two desktop applications
+
+send.c
+
+*/
+
+#include <stdio.h>
+#include <dbus/dbus.h>
+
+/*
+DBUS_EXPORT 
+DBusMessage * dbus_message_new_signal(const char * path,
+const char * iface,
+const char * name 
+);
+
+Constructs a new message representing a signal emission.
+
+Returns NULL if memory can't be allocated for the message. A signal is
+identified by its originating object path, interface, and the name of the
+signal.
+
+Path, interface, and signal name must all be valid (the D-Bus specification
+defines the syntax of these fields).
+
+Parameters
+path    the path to the object emitting the signal
+iface   the interface the signal is emitted from
+name    name of the signal
+
+Returns
+a new DBusMessage, free with dbus_message_unref()
+*/
+
+static void
+send_config(DBusConnection *connection)
+{
+  DBusMessage *message;
+
+  message = dbus_message_new_signal ("/org/share/linux",
+      "org.share.linux",
+      "Config");
+
+  /* Send the signal */
+  dbus_connection_send (connection, message, NULL);
+  dbus_message_unref (message);
+}
+
+static void
+send_customise(DBusConnection *connection)
+{
+  DBusMessage *message;
+
+  message = dbus_message_new_signal ("/org/share/linux",
+      "org.share.linux",
+      "Customize");
+
+  /* Send the signal */
+  dbus_connection_send (connection, message, NULL);
+  dbus_message_unref (message);
+}
+
+static void
+send_quit (DBusConnection *connection)
+{
+  DBusMessage *message;
+
+  message = dbus_message_new_signal ("/org/share/linux",
+      "org.share.linux",
+      "Quit");
+
+  /* Send the signal */
+  dbus_connection_send (connection, message, NULL);
+  dbus_message_unref (message);
+}
+
+int
+main (int argc, char **argv)
+{
+  DBusConnection *connection;
+
+  DBusError error;
+
+  dbus_error_init (&error);
+
+  connection = dbus_bus_get (DBUS_BUS_SESSION, &error);
+  if (!connection)
+  {
+    printf ("Failed to connect to the D-BUS daemon: %s", error.message);
+    dbus_error_free (&error);
+    return 1;
+  }
+
+  if ( argc == 1 )
+  {
+    return 0;
+  }
+
+  int i;
+
+  for ( i = 1; i < argc; i++)
+  {
+    if (!strcmp(argv[i],"-c") )
+    {
+      send_config(connection);
+    }
+    if (!strcmp(argv[i],"-u") )
+    {
+      send_customise(connection);
+    }
+    else if ( !strcmp(argv[i],"-q") )
+    {
+      send_quit(connection);
+    }
+  }
+
+  return 0;
+}
---
+++ code-linux/dbus/dbus-03.c
@@ -0,0 +1,67 @@
+/* 
+https://linoxide.com/how-tos/d-bus-ipc-mechanism-linux/
+
+Example 3: DBus services
+
+The message bus can start applications (services) on behalf of other
+applications, the application asks DBus to start a service by its name, usually
+the name should be known such as org.freedesktop.TextEditor.
+
+In order for DBus to find the executable corresponding to a particular name, the
+bus daemon looks for service description files which usually are installed in
+/usr/share/dbus-1/services and they have .service in their extension name (all
+linux distros that i know they use this prefix to install dbus services files),
+as an example of a service file.
+
+DBus service file example:
+
+[D-BUS Service]
+Name=org.share.linux
+Exec=path to the executable.
+
+keitee@kit-hdebi:/usr/share/dbus-1/services$ more org.bluez.obex.service
+[D-BUS Service]
+Name=org.bluez.obex
+Exec=/usr/lib/bluetooth/obexd
+SystemdService=obex.service
+
+We will write two programs, one is the service that we want to start the other
+is the application that activates this service
+
+share-linux-serivce-example.c
+
+$ gcc `pkg-config --libs --cflags dbus-1` dbus-03.c -o dbus-03-service
+$ gcc `pkg-config --libs --cflags dbus-1` dbus-03-start.c -o dbus-03-start
+
+*/
+
+#include <stdio.h>
+#include <dbus/dbus.h>
+
+int main()
+{
+  DBusConnection *connection;
+
+  DBusError error;
+
+  dbus_error_init(&error);
+
+  /* DBUS_BUS_STARTER is the bus that started us */
+  connection = dbus_bus_get(DBUS_BUS_STARTER, &error); 
+
+  // Do something here to make sure that the application was successfully
+  // started by DBus
+  // Example could be something like
+  
+  printf("share-linux-serivce-example.c is launched...\n");
+  FILE *tmp;
+  tmp = fopen("/tmp/share-linux-service.result", "w");
+  fprintf(tmp,"share-linux service was started successfully");
+  fclose(tmp);
+
+  // After that you have the service up, so you can do whetever you like 
+
+  dbus_connection_unref(connection);
+
+  return 0;
+}
