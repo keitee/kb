757b2c7cedcf2b5acc259b3f5edbc2c6fe0f18a9
Kit Park <kit.park@sky.uk>
2019-09-12 18:57:45 +0100

    Thu 12 Sep 18:57:45 BST 2019 keitee-mint

--- code-cxx/cxx/cxx_ex.cpp
+++ code-cxx/cxx/cxx_ex.cpp
@@ -22,6 +22,54 @@ TEST(Ex, ex)
   EXPECT_THAT(true, true);
 }
 
+namespace cxx_pattern_dispatcher
+{
+  template<typename T>
+    class Notifier : virtual public Polymorphic
+  {
+    public:
+      Notifier() {}
+
+      void add_observer(std::shared_ptr<T> const &o)
+      {
+        std::lock_guard<std::mutex> lock(m_);
+        observers_.push_back(o);
+      }
+
+      void remove_observer(std::shared_ptr<T> const &o)
+      {
+        std::unique_lock<std::mutex> lock(m_);
+
+        for (size_t i = 0; i < observers_.size(); ++i)
+        {
+          // get strong shared ptr from weak
+          // if stored strong pointer is still valid and which means object is
+          // still around and it's safe to use
+          if (observers_[i].lock() == o)
+          {
+            observers_.erase(observers_.begin() + i);
+
+            // this make to erase one. what if there are duplicates??
+            break;
+          }
+        }
+
+        // when observer calls this to remove and notifier is running then
+        // cannot remove it now. hold it for now until notifier finishes
+        if (notifying_)
+        {
+          waitee_count_++;
+
+          do
+          {
+          } while (waitee_count_);
+        }
+      }
+
+    private:
+      std::deque<std::weak_ptr<T>> observers_;
+  };
+} // namespace
 
 // ={=========================================================================
 
