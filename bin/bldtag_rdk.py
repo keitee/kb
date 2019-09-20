1821d35c92561c6e7c770b9b853c5e7879331cbc
Kit Park <kit.park@sky.uk>
2019-09-20 16:49:40 +0100

    Fri 20 Sep 16:49:40 BST 2019 keitee-mint

---
+++ bin/bldtag_rdk.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    # execute only if run as a script
+
+    # cleanup files
+    subprocess.call(["rm", "-f", "G* tags flist.out"])
+
+    # runs on /home/keitee/rdk/appinfrastructure/RDK
+    # dirs to include
+    dirs = [
+        'BluetoothRcu/daemon',
+        'BluetoothRcu/tools'
+        ]
+
+    listfile = 'flist.out'
+    logfile = open(listfile, 'w+')
+
+    # dirs to exclude
+    # find {to be replaced} -type d \( -path '*/build' -o -path '*/mock' \) -prune -o -print
+    # find BluetoothRcu/daemon -type d \( -path '*/build' -o -path '*/mock' \) -prune -o -print
+    # subprocess.call(["find", ".", "-type d \( -path '*/build' -o -path '*/mock' \)", "-prune", "-o", "-print"])
+    command = ["find", "-L", ".", "-type", "d", 
+        "(", 
+        # "-path",  "*/build", "-o", 
+        "-path",  "*/build",
+        # note on this end entry which is differnt from the above
+        # "-path", "*/test",
+        ")", "-prune", "-o", "-type", "f", "-print"]
+
+    for e in dirs:
+      # replace arg[2] for starting dir
+      command[2] = e
+      print command
+      subprocess.call(command, stdout=logfile)
+
+    logfile.close()
+
+    sys.stdout.write("building ctags for %s ...\n" % listfile)
+    subprocess.call(["ctags", "-L", listfile])
+
+    sys.stdout.write("building gtags for %s ...\n" % listfile)
+    subprocess.call(["gtags", "--skip-unreadable", "-f", listfile])
+
+    sys.stdout.write("done\n")
