a1511dca60aa4da8d03b318d14925e480b8632e4
Kit Park <kit.park@sky.uk>
2019-11-19 22:07:25 +0000

    Tue 19 Nov 22:07:25 GMT 2019 kit-hdebi

--- code-cxx/dbus/sdbus/dbus/dbusconnection.cpp
+++ code-cxx/dbus/sdbus/dbus/dbusconnection.cpp
@@ -5,13 +5,44 @@
   DBusConnectionPrivate
 */
 
+DBusConnectionPrivate::DBusConnectionPrivate(EventLoop const &eventLoop, sd_bus *bus)
+  : m_eventLoop(eventLoop)
+    , m_bus(bus)
+{} 
+
+
 /* ={--------------------------------------------------------------------------
  @brief :
   DBusConnection
 */
 DBusConnection::DBusConnection(std::shared_ptr<DBusConnectionPrivate> &&priv)
+  : m_private(std::move(priv))
 {
 }
+
+// https://www.freedesktop.org/software/systemd/man/sd_bus_attach_event.html#
+//
+// sd_bus_attach_event, sd_bus_detach_event, sd_bus_get_event 
+// — Attach a bus connection object to an event loop
+//
+// sd_bus_attach_event() attaches the specified bus connection object to an
+// sd-event(3) event loop object at the specified priority (see
+// sd_event_source_set_priority(3) for details on event loop priorities). 
+//
+// When a bus connection object is attached to an event loop incoming messages
+// will be automatically read and processed, and outgoing messages written,
+// whenever the event loop is run. When the event loop is about to terminate,
+// the bus connection is automatically flushed and closed (see
+// sd_bus_set_close_on_exit(3) for details on this). 
+//
+// By default bus connection objects are not attached to any event loop. When a
+// bus connection object is attached to one it is not necessary to invoke
+// sd_bus_wait(3) or sd_bus_process(3) as this functionality is handled
+// automatically by the event loop.
+//
+// see sdbus_server_1.c example
+
+
 DBusConnection DBusConnection::systemBus(EventLoop const &eventLoop)
 {
   sd_bus *bus{nullptr};
@@ -31,5 +62,7 @@ DBusConnection DBusConnection::systemBus(EventLoop const &eventLoop)
     sd_bus_unref(bus);
     return DBusConnection(nullptr);
   }
+
+  return DBusConnection(std::make_shared<DBusConnectionPrivate>(eventLoop, bus));
 }
 
